88274c47728f69f3966945421f14309834e2c7bf
RyanKilbane <RyanKilbane92@hotmail.com>
2019-09-20 18:02:30 +0100

    Added ajax to add page

--- add_repo/add_repo.py
+++ add_repo/add_repo.py
@@ -8,14 +8,13 @@ add_repo_blueprint = Blueprint(name="add_repo",import_name=__name__, url_prefix=
 @add_repo_blueprint.route("/add", methods=["POST"])
 def add_repo():
     new_repo = request.args.get("repo")
-    if isinstance(new_repo, type(None)):
-        return ("No repo passed", 400)
+    if new_repo == "":
+        return (json.dumps({"message": "No repo passed"}), 400)
     try:
         git.add_repo(new_repo)
-        print(git.repos)
-        return ("Added", 200)
+        return (json.dumps({"message": "Repo added"}), 200)
     except IntegrityError as error:
-        return (json.dumps({"error": "Repo already exists"}), 500)
+        return (json.dumps({"message": "Repo already exists"}), 500)
 
 @add_repo_blueprint.route("/add", methods=["GET"])
 def get_page():
