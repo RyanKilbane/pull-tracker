0ba3c6e41fd37dc78cbe7e19bc7106aae5b18762
RyanKilbane <RyanKilbane92@hotmail.com>
2019-09-20 12:57:01 +0100

    For some reason the build json class wasn't working. Fixed

--- tests/test_build_json.py
+++ tests/test_build_json.py
@@ -1,19 +1,20 @@
 from utils.build_json import build_json
 
 class DummyRepo:
-    def __init__(self, issue, name):
-        self.issue = issue
-        self.name = name
+    def __init__(self, issue_name, repo_name):
+        self.title = issue_name
+        self.repo = repo_name
+
     def get_issues(self):
         return self.issue
-    def get_name(self):
-        return self.name
 
-dummy = DummyRepo("an issue", "a repo")
+    def title(self):
+        return self.title
+
 
 def test_build_issue_json():
     assert build_json("A Repo") == {"A Repo": {}}
 
 def test_build_issue_json_with_issues():
-    assert build_json("A Repo", issues=["issue1", "issue2"]) == {"A Repo": [{"issue_name": "issue1"}, {"issue_name": "issue2"}]}
+    assert build_json("A Repo", issues=[DummyRepo("issue1", "A repo"), DummyRepo("issue2", "A repo")]) == {"A Repo": [{"issue_name": "issue1"}, {"issue_name": "issue2"}]}
     
