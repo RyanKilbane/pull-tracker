0dbc241814b2d4e52f2f88ab721da8f0de282c4e
RyanKilbane <RyanKilbane92@hotmail.com>
2019-09-24 21:46:53 +0100

    Added new build_json_threads function so that issues can be gotten concurrently

--- track_issues/track_issues.py
+++ track_issues/track_issues.py
@@ -1,17 +1,16 @@
 from repos.repo import git
-from utils.build_json import build_json
+from utils.build_json import build_json, build_json_threads
 from utils.filter_no_issues import filter_issues
 from flask import Blueprint
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 track_issues = Blueprint(name="track_issues", import_name=__name__, url_prefix="/")
 
 @track_issues.route("/pulls")
 def issues():
     repos = git.get_repos()
-    open_issues = []
-    for repo in repos:
-        open_issues.append(build_json(repo.name, issues=repo.get_pulls(state="open"), issue_type="pull"))
-    print(open_issues)
-    # output_issues = filter_issues(open_issues)
+    with ThreadPoolExecutor(max_workers=5) as Executor:
+        x = Executor.map(build_json_threads, repos)
+    open_issues = list(x)
     return (json.dumps(open_issues), 200)
--- utils/build_json.py
+++ utils/build_json.py
@@ -6,3 +6,9 @@ def build_json(repo, issues=None, issue_type="issue"):
     return {repo: [{f"{issue_type}_name": issue.title, 
                     f"{issue_type}_number": issue.number, 
                     "reviewers": get_user_details(get_reviewers(issue.number, "ONSDigital", repo))} for issue in issues]}
+
+def build_json_threads(repo, issue_type="pull"):
+    issues = repo.get_pulls(state="open")
+    return {repo.name: [{f"{issue_type}_name": issue.title, 
+                    f"{issue_type}_number": issue.number, 
+                    "reviewers": get_user_details(get_reviewers(issue.number, "ONSDigital", repo.name))} for issue in issues]}
